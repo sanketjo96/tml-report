e3a761c017768990af6e1db4381e8f088336ba04
synerzip <sanketjoshi96@gmail.com>
2019-06-20 14:48:19 +0530

    update file check API

--- routes/fileupload.js
+++ routes/fileupload.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const readReportFile = require('../src/common').readReportFile;
+const dataSet = require('../src/common').dataSet;
 const ResponseData = require('../modals/response');
 const router = new express.Router();
+
+router.get('/checkfile', async (req, res) => {
+    let responseData = new ResponseData();
+    const records = dataSet.length;
+    let message = 'File upload is required';
+    if (records) {
+        message = `File exists with ${records} records`;
+    }
+    responseData.setData([], message);
+    res.send(responseData);
+});
+
 
 router.get('/addfile', async (req, res) => {
     let responseData = new ResponseData();
--- test/fileupload.test.js
+++ test/fileupload.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest');
 const app = require('../src/app');
 
+test('file is not present initially', async () => {
+    const response = await request(app).get('/checkfile').expect(200);
+    expect(response.body.message).toEqual('File upload is required')
+});
+
 test('Connection for upload the file', async () => {
     const response = await request(app).get('/addfile').expect(200);
     expect(response.body.message).toEqual('Data has been fed succesfuly!!')
